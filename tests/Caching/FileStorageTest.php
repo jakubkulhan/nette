323426fdd8d2f867185826c587ef91e506814b7d
Jakub Kulhan <jakub.kulhan@gmail.com>
2010-08-19 22:15:16 +0200

    Fixed FileStorageTest
    Git doesn't track empty directories, so one have to be created before FileStorageTest
    is run.

--- tests/Caching/FileStorageTest.php
+++ tests/Caching/FileStorageTest.php
@@ -14,6 +14,10 @@ class FileStorageTest extends CacheStorageTest
 {
 	protected function createStorage()
 	{
+		if (!is_dir(__DIR__ . '/../temp')) {
+			mkdir(__DIR__ . '/../temp', 0755);
+		}
+
 		return new FileStorage(__DIR__ . '/../temp');
 	}
 }
